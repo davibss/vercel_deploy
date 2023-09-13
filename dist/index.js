21a0ebde512097d275b284d429f109a6f9ad0375
davibss <davi.sousa@ccc.ufcg.edu.br>
2023-09-12 21:16:41 -0300

    fix: change require to import

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,69 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const generator = require("@asyncapi/generator");
+// @ts-ignore
+const generator_1 = __importDefault(require("@asyncapi/generator"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
-app.get('/', (_req, res) => {
+const spec = `
+asyncapi: "2.5.0"
+info:
+  title: Demo API
+  version: "1.0.0"
+channels:
+  channelOne:
+    publish:
+      summary: This is the first sample channel
+      operationId: onMessage
+      message:
+        name: FirstMessage
+        payload:
+          id:
+            type: integer
+            minimum: 0
+            description: Id of the channel
+          sentAt:
+            type: string
+            format: date-time
+            description: Date and time when the message was sent.
+  channelTwo:
+    publish:
+      summary: This is the second sample channel
+      operationId: messageRead
+      message:
+        name: SecondMessage
+        payload:
+          id:
+            type: integer
+            minimum: 0
+            description: Id of the channel
+          sentAt:
+            type: string
+            format: date-time
+            description: Date and time when the message was sent.
+`;
+app.get('/', (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
     return res.send('Express Typescript on Vercel');
-});
-app.get('/ping', (_req, res) => {
+}));
+app.get('/ping', (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const generatorInstance = new generator_1.default("@asyncapi/html-template", "/tmp", {
+        forceWrite: true
+    });
+    yield generatorInstance.generateFromString(spec);
     return res.send('pong 🏓');
-});
+}));
 app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,61 @@
 import express, { Request, Response } from 'express';
-const generator = require("@asyncapi/generator");
+// @ts-ignore
+import generator from "@asyncapi/generator";
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', (_req: Request, res: Response) => {
+const spec = `
+asyncapi: "2.5.0"
+info:
+  title: Demo API
+  version: "1.0.0"
+channels:
+  channelOne:
+    publish:
+      summary: This is the first sample channel
+      operationId: onMessage
+      message:
+        name: FirstMessage
+        payload:
+          id:
+            type: integer
+            minimum: 0
+            description: Id of the channel
+          sentAt:
+            type: string
+            format: date-time
+            description: Date and time when the message was sent.
+  channelTwo:
+    publish:
+      summary: This is the second sample channel
+      operationId: messageRead
+      message:
+        name: SecondMessage
+        payload:
+          id:
+            type: integer
+            minimum: 0
+            description: Id of the channel
+          sentAt:
+            type: string
+            format: date-time
+            description: Date and time when the message was sent.
+`;
+
+app.get('/', async (_req: Request, res: Response) => {
     return res.send('Express Typescript on Vercel');
 });
 
-app.get('/ping', (_req: Request, res: Response) => {
+app.get('/ping', async (_req: Request, res: Response) => {
+    const generatorInstance = new generator(
+        "@asyncapi/html-template", 
+        "/tmp", 
+        {
+            forceWrite: true
+        }
+    );
+    await generatorInstance.generateFromString(spec);
     return res.send('pong 🏓');
 });
 
